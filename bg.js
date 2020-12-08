1bcc2b55cc164d83e3a9a9ff32a07c611f54be94
Nishant <nishantsingh2jan1998@gmail.com>
2020-12-08 21:40:18 +0530

    coronaVirus Extensions

---
+++ bg.js
@@ -0,0 +1,17 @@
+replaceText(document.body)
+function replaceText(element){
+
+	if(element.hasChildNodes()){
+
+		element.childNodes.forEach(replaceText)
+	}
+	else if(element.nodeType === Text.TEXT_NODE){
+
+		if (element.textContent.match(/coronavirus/gi)) {
+
+
+			element.parentElement.style.color="black"
+			element.parentElement.style.backgroundColor="black"
+		}
+	}
+	}
